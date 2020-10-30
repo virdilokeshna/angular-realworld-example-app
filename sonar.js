80086a15193118a635758af416a836e4383dba9a
virdilokeshna <71276942+virdilokeshna@users.noreply.github.com>
2020-10-30 14:55:35 +0530

    sonar-scanner

---
+++ sonar.js
@@ -0,0 +1,8 @@
+const sonarqubeScanner = require('sonarqube-scanner');
+     sonarqubeScanner({
+       serverUrl: 'http://sonarqubeprac.eastus2.cloudapp.azure.com:9000',
+       options : {
+       'sonar.sources': '.',
+       'sonar.inclusions' : 'src/**' // Entry point of your code
+       }
+     }, () => {});
